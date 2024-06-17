70c4a6580a26e8c827d8dd638f5a7a7a6a99b59d
Shpetim Selaci <shselaci3@gmail.com>
2024-06-17 21:45:35 +0200

    Fix backend Dockerfile & docker-compose.yml so I can whether things are working

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import config from '../config';
 import * as schema from './schemas';
+
 const pool = new Pool(config.db);
 
 const db = drizzle(pool, { schema });
