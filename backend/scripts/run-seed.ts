59382ccb04456b7306658c9097e3c13316c1d580
Shpetim Selaci <shselaci3@gmail.com>
2024-06-18 12:03:04 +0200

    Fix seed to include ids that are from insers

--- backend/scripts/run-seed.ts
+++ backend/scripts/run-seed.ts
@@ -32,25 +32,25 @@ const runSeed = async () => {
         name: faker.person.fullName(),
       });
     }
-    await db.insert(schema.users).values(users);
+    const insertedUsers = await db.insert(schema.users).values(users).returning();
 
     const questions: schema.NewQuestion[] = [];
     for (let i = 0; i < 20; i++) {
       questions.push({
         title: faker.lorem.words({ min: 1, max: 5 }),
         description: faker.lorem.sentences({ min: 1, max: 5 }),
-        authorId: Math.round(Math.random() * 19),
+        authorId: insertedUsers[Math.round(Math.random() * 19)].id,
       });
     }
-    await db.insert(schema.questions).values(questions);
+    const insertedQuestions = await db.insert(schema.questions).values(questions).returning();
 
     const answers: schema.NewAnswer[] = [];
     for (let i = 0; i < 20; i++) {
       for (let i = 0; i < Math.random() * 20; i++) {
         answers.push({
-          questionId: i,
+          questionId: insertedQuestions[Math.round(Math.random() * 19)].id,
           answer: faker.lorem.sentences({ min: 1, max: 5 }),
-          creatorId: Math.round(Math.random() * 19),
+          creatorId: insertedUsers[Math.round(Math.random() * 19)].id,
         });
       }
     }
