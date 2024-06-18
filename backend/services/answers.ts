8c5de594a770470a03fd045a31bd692d6857d715
Shpetim Selaci <shselaci3@gmail.com>
2024-06-18 12:35:18 +0200

    Fix listQuestionAnswers query

--- backend/services/answers.ts
+++ backend/services/answers.ts
@@ -1,7 +1,7 @@
 import { and, eq, getTableColumns } from 'drizzle-orm';
 import { DEFAULT_LIMIT } from '../constants';
 import db from '../db';
-import { PaginationParams } from '../common/types';
+import { NonNullableObject, PaginationParams } from '../common/types';
 import { answers, users, questions, NewAnswer, Answer } from '../db/schemas';
 import { alias } from 'drizzle-orm/pg-core';
 
@@ -13,23 +13,36 @@ export const listQuestionAnswers = async ({
   const authors = alias(users, 'author');
 
   const { authorId, ...questionColumns } = getTableColumns(questions);
-  const questionAnswers = await db
-    .select({
-      id: answers.id,
-      answer: answers.answer,
-      author: getTableColumns(authors),
-      question: questionColumns,
-      createdAt: answers.createdAt,
-      updatedAt: answers.updatedAt,
-    })
-    .from(answers)
-    .where(and(eq(answers.questionId, id), eq(answers.deleted, false)))
-    .leftJoin(questions, eq(answers.questionId, questions.id))
-    .rightJoin(authors, eq(answers.creatorId, users.id))
-    .offset(offset || 0)
-    .limit(limit || DEFAULT_LIMIT);
 
-  return questionAnswers as Required<typeof questionAnswers>;
+  const questionAnswers = await db.query.answers.findMany({
+    columns: {
+      id: true,
+      answer: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    where: () =>
+      and(eq(answers.questionId, id), eq(answers.deleted, false), eq(questions.deleted, false)),
+    with: {
+      author: true,
+      question: {
+        with: {
+          author: true,
+        },
+        columns: {
+          id: true,
+          title: true,
+          description: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+    },
+    offset: offset || 0,
+    limit: limit || DEFAULT_LIMIT,
+  });
+
+  return questionAnswers as NonNullableObject<typeof questionAnswers>;
 };
 
 export const addAnswer = async (newUser: Omit<NewAnswer, 'createdAt' | 'updatedAt'>) => {
